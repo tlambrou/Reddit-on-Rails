7d5ebf7474767c1d0ba928061c6ae31b5b785b07
tlambrou <lambrou@gmail.com>
2016-11-18 16:11:38 -0800

    Added the subs

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -1,2 +1,27 @@
 class SubsController < ApplicationController
+
+  def new
+     @sub = Sub.new
+  end
+
+  def create
+    @sub = Sub.new(sub_params)
+    if @sub.save
+      redirect_to @sub
+    else
+      redirect_to @sub
+      flash[:notice] = "Subreddit could not be saved."
+    end
+  end
+
+  def show
+    @sub = Sub.find(params[:id])
+  end
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def sub_params
+      params.require(:sub).permit(:name)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  # resources :subs, only: [:new, :create, :show]
-  resources :posts, only: [:index, :new, :create, :show] do
+  resources :subs, only: [:new, :create, :show] do
+    resources :posts, only: [:index, :new, :create, :show]
+  end
+
+  resources :posts, only: [:show] do
     resources :comments, only: [:create]
   end
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
